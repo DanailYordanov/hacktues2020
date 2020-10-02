847ab92aadb644cf41a033c3ff31f51a6f4e152a
shefanaTues <kaloyan.georgieev@gmail.com>
2020-10-02 23:31:44 +0300

    Create event creator page

--- hacktues/main/urls.py
+++ hacktues/main/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('volunteer_login', views.volunteer_page, name='main-volunteer_page'),
     path('user_login', views.user_page, name='main-user_page'),
     path('volunteer', views.volunteer_profile, name='main-volunteer_profile'),
-    path('volunteer/create_event', views.create_event, name='main-volunteer_profile/create_event'),
+    path('create_event', views.create_event, name='main-create_event'),
 ]
