0bbcd9ecf084e3241dcbe1b17f9f5ef2c4b689e0
shefanaTues <kaloyan.georgieev@gmail.com>
2020-10-02 17:06:15 +0300

    Add some html

--- hacktues/main/urls.py
+++ hacktues/main/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index, name='main-index')
+    path('', views.index, name='main-index'),
+    path('volunteer_page', views.volunteer_page, name='main-volunteer_page'),
+    path('user_page', views.user_page, name='main-user_page')
 ]
--- hacktues/main/views.py
+++ hacktues/main/views.py
@@ -3,3 +3,8 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'main/index.html')
+
+def volunteer_page(request):
+    return render(request, 'main/volunteer_page.html')
+def user_page(request):
+    return render(request, 'main/user_page.html')
