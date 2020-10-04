cb81bfcd8a22332b777a7ba0ea355ef022de379d
Danail Yordanov <danail.yordanov1@gmail.com>
2020-10-05 02:31:10 +0300

    Delete unnecessary stuff

--- hacktues/main/views.py
+++ hacktues/main/views.py
@@ -67,34 +67,6 @@ def room(request, pk):
         raise PermissionDenied
 
 
-def room_user(request):
-    context = {
-        'users': [
-            'Пенка Георгиева',
-            'Радка Тодорова',
-            'Васил Тодоров',
-            'Илияна Вълева'
-        ],
-        'events': [
-            {
-                'title': 'ПЪРВО СЪБИТИЕ!',
-                'author': 'Калоян',
-                'content': 'Ще посещавам Kaufland в квартала на 6 октомври около 12 часа. Ако имате заявки пратете списък!',
-                'id': 1,
-                'date_posted': '4 Октомври 2020 17:39'
-            },
-            {
-                'title': 'Събитие второ',
-                'author': 'Дани',
-                'content': 'Това е второто събитие',
-                'id': 2,
-                'date_posted': '2 Октомври 2020 14:56'
-            }
-        ]
-    }
-    return render(request, 'main/room_user.html', context)
-
-
 def choice(request):
     return render(request, 'main/choice.html')
 
