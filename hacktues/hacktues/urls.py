cd03ccc2a0eff6aee42066f522155fb085726227
shefanaTues <kaloyan.georgieev@gmail.com>
2020-10-04 12:42:04 +0300

    Add login and register form for volunteer

--- hacktues/hacktues/urls.py
+++ hacktues/hacktues/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-
+from users import views as auth_views
 urlpatterns = [
     path('', include('main.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
+    path('register/', auth_views.register, name='register'),
+    path('login/', auth_views.login, name= 'login'),
 ]
--- hacktues/users/views.py
+++ hacktues/users/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
+def register(request):
+    return render(request, 'account/signup.html')
+def login(request):
+    return render(request, 'account/login.html')
